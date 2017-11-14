6b387af136e66179d4cbb392fd292eb8cc841426
Ryan Gordon <ryan@quizlet.com>
2017-11-14 14:55:24 -0800

    Spread expiry out

--- src/CacheTrait.php
+++ src/CacheTrait.php
@@ -60,7 +60,8 @@ trait CacheTrait
         $cacheItem->set($v);
         if ($expiry !== null) {
             \QMetric::increment('spanner.app_time.oauth_token.set_cached_value.count_expiry');
-            $cacheItem->expiresAfter($expiry);
+            // Spread the renewal out between 5 seconds before and 600 seconds before it will actually expire
+            $cacheItem->expiresAfter($expiry - mt_rand(5, 600));
         } else {
             \QMetric::increment('spanner.app_time.oauth_token.set_cached_value.count_lifetime');
             $cacheItem->expiresAfter($this->cacheConfig['lifetime']);
